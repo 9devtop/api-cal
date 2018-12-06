0a06ac23f0cac1e24a4bc343f6dc19366dd72564
Tan <tan@think@.co.nz>
2018-12-06 11:29:43 +0700

    init

---
+++ CalculatorAPI/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("cong/{a}/{b}")]
+        public ActionResult<int> Get(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
